b75c7e308dd0f876556fa8e4fe48362286286f5b
mo <vitch.michhh@gmail.com>
2025-06-05 19:50:40 +0300

    change

--- app.js
+++ app.js
@@ -33,9 +33,12 @@ app.get("/", (req, res) => {
 // app.options("*", cors()); // Enable preflight for all routes
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["http://localhost:4200"]);
+  res.header("Access-Control-Allow-Origin", [
+    "server-eta-amber.vercel.app"
+
+  ]);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", ["Content-Type, Authorization"] );
+  res.header("Access-Control-Allow-Headers", ["Content-Type, Authorization"]);
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
