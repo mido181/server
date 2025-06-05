62c305e803531b05138c5ec10a2d40ae88799fb1
mo <vitch.michhh@gmail.com>
2025-06-05 20:13:59 +0300

    tet

--- app.js
+++ app.js
@@ -33,10 +33,7 @@ app.get("/", (req, res) => {
 // app.options("*", cors()); // Enable preflight for all routes
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "https://muslima-henna.vercel.app"
-
-  ]);
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", ["Content-Type, Authorization"]);
   res.header("Access-Control-Allow-Credentials", "true");
